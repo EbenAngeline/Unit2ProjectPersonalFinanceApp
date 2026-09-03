74c167852788abf43a28d873a8fa05a98672dd47
Eben Angeline <ebenangeline6@gmail.com>
2026-09-03 14:50:16 -0500

    Init Project

---
+++ src/main/java/com/example/Unit2ProjectPersonalFinanceApp/Unit2ProjectPersonalFinanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Unit2ProjectPersonalFinanceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit2ProjectPersonalFinanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit2ProjectPersonalFinanceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Unit2ProjectPersonalFinanceApp/Unit2ProjectPersonalFinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Unit2ProjectPersonalFinanceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit2ProjectPersonalFinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
